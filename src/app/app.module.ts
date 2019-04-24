7092633674b877e1d239eee402a9e651217cd577
Misk <mihajlo.jovkovic@gmail.com>
2019-04-24 05:21:07 +0200

    Model znatno unapređen, dodata osnova frontend-a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { ErrorHandler, NgModule} from '@angular/core';
+import {RouterModule} from "@angular/router";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms"
 import { AppRoutingModule } from './app-routing.module';
+
+
+
+
+
+import {AirlineCreate} from "./models/airlineCreate";
+
+;
+
+
+
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+
+import {AirlineService} from "./services/airline/airline.service";
+import {JwtService} from "./services/jwt.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path:'',
+        component: HomePageComponent
+      },
+      {
+        path:'home',
+        component:HomePageComponent
+      }
+    ],)
   ],
-  providers: [],
+  providers: [ AirlineService, JwtService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
